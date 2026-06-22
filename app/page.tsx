28e47ab82e49806779c539577a3229223b104924
Joy <elooghenejoy@gmail.com>
2026-06-22 20:20:38 +0100

    fetch data with tanstack query

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 interface Post {
@@ -9,22 +10,30 @@ interface Post {
 
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([])
 
   const fetchPosts = async () => {
     const response = await fetch('https://dummyjson.com/posts')
     const data = await response.json()
-    setPosts(data.posts)
+    return data.posts;
+  }
+  const { isPending, error, data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts
+  })
+
+  if (isPending) {
+    return <p>Loading...</p>
   }
 
-  useEffect(() => {
-    fetchPosts()
-  }, [])
+  if (error) {
+    return <p>Error: {error instanceof Error ? error.message : 'Unknown error'}</p>
+  }
+
 
   return (
  <div>
 <h1>Welcome to the Party!</h1>
-{posts.map((post) => (
+{data?.map((post: Post) => (
   <div key={post.id}>
     <h2>{post.title}</h2>
     <p>{post.body}</p>
